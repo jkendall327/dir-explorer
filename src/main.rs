401e1d7df18c2df2f65de4e0a2b4ae0038b332c3
jackkendall <jkendall3096@gmail.com>
2025-08-25 10:51:02 +0100

    Basic tracing

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,12 @@ use axum::{
 };
 use confy::ConfyError;
 use serde::{Deserialize, Serialize};
-use tower_http::services::ServeDir;
+use tower_http::{
+    services::ServeDir,
+    trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer},
+};
+use tracing::Level;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Template)]
 #[template(path = "home.html")]
@@ -40,8 +45,27 @@ fn load() -> Result<Settings, ConfyError> {
 async fn main() -> Result<(), Box<dyn Error>> {
     let config = load()?;
 
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "info,tower_http=info,axum=info".into()),
+        )
+        .with(tracing_subscriber::fmt::layer().compact())
+        .init();
+
+    let trace = TraceLayer::new_for_http()
+        .make_span_with(
+            DefaultMakeSpan::new()
+                .level(Level::INFO)
+                .include_headers(false),
+        )
+        .on_request(DefaultOnRequest::new().level(Level::INFO))
+        .on_response(DefaultOnResponse::new().level(Level::INFO));
+
     let app = Router::new()
         .route("/", get(home))
+        .route("/health", get(health))
+        .layer(trace)
         .nest_service("/static", ServeDir::new("static"));
 
     let listener = tokio::net::TcpListener::bind((config.address, config.port)).await?;
@@ -57,4 +81,8 @@ async fn home() -> impl IntoResponse {
         items: vec!["One", "Two", "Three"],
     };
     Html(tmpl.render().unwrap())
+}
+
+async fn health() -> &'static str {
+    "healthy"
 }
