bb82519d22d0185437bb0f7579f9fe9082ada40d
jackkendall <jkendall3096@gmail.com>
2025-08-25 10:37:40 +0100

    Config

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use std::error::Error;
+
 use askama::Template;
 use axum::{
     Router,
     response::{Html, IntoResponse},
     routing::get,
 };
+use confy::ConfyError;
+use serde::{Deserialize, Serialize};
 use tower_http::services::ServeDir;
 
 #[derive(Template)]
@@ -13,16 +17,38 @@ struct HomeTemplate<'a> {
     items: Vec<&'a str>,
 }
 
+#[derive(Serialize, Deserialize)]
+struct Settings {
+    port: u16,
+    address: String,
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Self {
+            port: 3000,
+            address: "0.0.0.0".to_owned(),
+        }
+    }
+}
+
+fn load() -> Result<Settings, ConfyError> {
+    confy::load::<Settings>("myapp", None)
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
+    let config = load()?;
+
     let app = Router::new()
         .route("/", get(home))
         .nest_service("/static", ServeDir::new("static"));
 
-    let listener = tokio::net::TcpListener::bind(("0.0.0.0", 3000))
-        .await
-        .unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind((config.address, config.port)).await?;
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
 
 async fn home() -> impl IntoResponse {
